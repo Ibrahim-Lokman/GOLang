9a34fd605223b7c029dd746bfd7642b26590c44c
Ibrahim-Lokman <ibrahimlokman.bd@gmail.com>
2021-11-05 19:16:20 +0600

    2

--- codes/002-variable-datatype/varkeywords.go
+++ codes/002-variable-datatype/varkeywords.go
@@ -2,8 +2,15 @@ package main
 
 import "fmt"
 
+var a = 120
+
 func main() {
 	var y = 43 * 2
 	fmt.Println(y)
-
+	fmt.Println(a)
+	print()
+}
+func print() {
+	// fmt.Println(y) not accessable
+	fmt.Println(a)
 }
