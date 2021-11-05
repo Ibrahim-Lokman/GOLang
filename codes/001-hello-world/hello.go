663de4e3a2f7ddcdff12ebbdb3b251bab31d9d39
Ibrahim-Lokman <ibrahimlokman.bd@gmail.com>
2021-11-05 16:50:16 +0600

    'practise 5/11/2021'

--- codes/001-hello-world/hello.go
+++ codes/001-hello-world/hello.go
@@ -3,5 +3,16 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello Sajeet, It's 17/10/2021, 2:35 pm")
+	//fmt.Println("Hello Sajeet, It's 17/10/2021, 2:35 pm")
+	fmt.Println("Hello World")
+	print()
+	for i := 0; i < 100; i++ {
+		if i%2 == 0 {
+			print()
+			fmt.Println(i)
+		}
+	}
+}
+func print() {
+	fmt.Println("Hello Sajeet")
 }
